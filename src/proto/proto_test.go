0b2935516014c69946bea4b1445721b69e4ac913
rafaelputaro <rafaelputaro@gmail.com>
2025-04-11 19:20:23 -0300

    proto sin terminar

---
+++ src/proto/proto_test.go
@@ -0,0 +1,40 @@
+package proto
+
+import (
+	"testing"
+)
+
+func TestMovie(t *testing.T) {
+
+}
+
+/*
+
+
+1 False,
+2 "{'id': 10194, 'name': 'Toy Story Collection', 'poster_path': '/7G9915LfUQ2lVfwMEEhDsn3kT4B.jpg', 'backdrop_path': '/9FBwqcd9IRruEDUrTdcaafOMKUq.jpg'}",
+3 30000000,
+4 "[{'id': 16, 'name': 'Animation'}, {'id': 35, 'name': 'Comedy'}, {'id': 10751, 'name': 'Family'}]",
+5 http://toystory.disney.com/toy-story,
+6 862,
+7 tt0114709,
+8 en,
+9 Toy Story,
+10 "Led by Woody, Andy's toys live happily in his room until Andy's birthday brings Buzz Lightyear onto the scene. Afraid of losing his place in Andy's heart, Woody plots against Buzz. But when circumstances separate Buzz and Woody from their owner, the duo eventually learns to put aside their differences.",
+
+11 21.946943,
+12 /rhIRbceoE9lR4veEXuwCC2wARtG.jpg,
+13 "[{'name': 'Pixar Animation Studios', 'id': 3}]",
+14 "[{'iso_3166_1': 'US', 'name': 'United States of America'}]",
+15 1995-10-30,
+16 373554033,
+17 81.0,
+18 "[{'iso_639_1': 'en', 'name': 'English'}]",
+19 Released
+20 ,  ,
+21 Toy Story,
+22 False,
+23 7.7,
+24 5415
+
+**/
