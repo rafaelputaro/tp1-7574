e2ef68ebc1f85a93bd0aa6d6292f7033796979e6
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2025-04-24 03:21:37 -0300

    Report .protos

--- src/server/gateway/internal/jsonutil.go
+++ src/server/gateway/internal/jsonutil.go
@@ -13,13 +13,25 @@ var (
 // NormalizeJSON returns a cleaned version of a malformed JSON-like string.
 // If it's already valid JSON, it's returned untouched.
 func NormalizeJSON(input string) string {
-	var test interface{}
-	if json.Unmarshal([]byte(input), &test) == nil {
+	if isValid(input) {
 		return input
 	}
 
 	cleaned := strings.ReplaceAll(input, "'", `"`)
+	if isValid(cleaned) {
+		return cleaned
+	}
+
 	cleaned = reNoneLiteral.ReplaceAllString(cleaned, `: null$1`)
 
 	return cleaned
+}
+
+// isValid checks if a string is a valid JSON object.
+func isValid(s string) bool {
+	var js map[string]interface{}
+	if err := json.Unmarshal([]byte(s), &js); err != nil {
+		return false
+	}
+	return true
 }
