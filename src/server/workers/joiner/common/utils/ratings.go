26ed3d5a8ec17d777ffd37d0802de08a901bab73
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2025-06-18 20:30:35 -0300

    Fix

--- src/server/workers/joiner/common/utils/ratings.go
+++ src/server/workers/joiner/common/utils/ratings.go
@@ -8,54 +8,61 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type MovieData struct {
+type MovieInfo struct {
 	Title     string
 	RatingSum float32
 	Count     int64
 	RatingAvg float64
+	Ratings   map[int64]float32
 }
 
 type RatingTotalizer struct {
-	Movies []MovieData
-	Index  map[int64]int64 // key: movie id
+	Movies     map[int64]*MovieInfo
+	SeenMovies map[int64]struct{}
 }
 
-func newMovieData(title string, ratingSum float32, count int64, ratingAvg float64) *MovieData {
-	return &MovieData{
+func newMovieInfo(title string) *MovieInfo {
+	return &MovieInfo{
 		Title:     title,
-		RatingSum: ratingSum,
-		Count:     count,
-		RatingAvg: ratingAvg,
+		RatingSum: 0,
+		Count:     0,
+		RatingAvg: 0,
+		Ratings:   make(map[int64]float32),
 	}
 }
 
 func NewRatingTotalizer() *RatingTotalizer {
 	return &RatingTotalizer{
-		Movies: []MovieData{},
-		Index:  make(map[int64]int64),
+		Movies:     make(map[int64]*MovieInfo),
+		SeenMovies: make(map[int64]struct{}),
 	}
 }
 
-// append a movie
 func (totalizer *RatingTotalizer) AppendMovie(movie *protopb.MovieSanit) {
-	totalizer.Index[int64(*movie.Id)] = int64(len(totalizer.Movies))
-	totalizer.Movies = append(totalizer.Movies, *newMovieData(*movie.Title, 0.0, 0, 0.0))
+	movieID := int64(movie.GetId())
+
+	totalizer.SeenMovies[movieID] = struct{}{}
+
+	if _, exists := totalizer.Movies[movieID]; !exists {
+		totalizer.Movies[movieID] = newMovieInfo(movie.GetTitle())
+	} else {
+		totalizer.Movies[movieID].Title = movie.GetTitle()
+	}
 }
 
-// update rating for a movie
 func (totalizer *RatingTotalizer) Sum(rating *protopb.RatingSanit) {
-	index, exists := totalizer.Index[*rating.MovieId]
-	if exists {
-		founded := totalizer.Movies[index]
-		// update
-		newData := *newMovieData(
-			founded.Title,
-			founded.RatingSum+*rating.Rating,
-			founded.Count+1,
-			0.0,
-		)
-		totalizer.Movies[index] = newData
+	movieID := rating.GetMovieId()
+	ratingValue := rating.GetRating()
+
+	if _, exists := totalizer.Movies[movieID]; !exists {
+		totalizer.Movies[movieID] = newMovieInfo("")
 	}
+
+	ratingID := int64(len(totalizer.Movies[movieID].Ratings) + 1)
+	totalizer.Movies[movieID].Ratings[ratingID] = ratingValue
+
+	totalizer.Movies[movieID].RatingSum += ratingValue
+	totalizer.Movies[movieID].Count++
 }
 
 func CreateTopAndBottomRatingAvgEof(clientID string, messageId int64, sourceId string) *protopb.TopAndBottomRatingAvg {
@@ -71,7 +78,6 @@ func CreateTopAndBottomRatingAvgEof(clientID string, messageId int64, sourceId s
 	}
 }
 
-// Returns string from TopAndBottomRagingAvg
 func TopAndBottomToString(topAndBottom *protopb.TopAndBottomRatingAvg) string {
 	return fmt.Sprintf("Top: %s(%v) | Bottom: %s(%v)",
 		*topAndBottom.TitleTop, *topAndBottom.RatingAvgTop,
@@ -79,11 +85,11 @@ func TopAndBottomToString(topAndBottom *protopb.TopAndBottomRatingAvg) string {
 }
 
 // Returns: 1 rating1 < rating2; 0 rating1 == rating2; -1 rating1 > rating2
-func cmpMovieData(data1, data2 MovieData) int {
-	if data1.RatingAvg == data2.RatingAvg {
+func cmpMovieInfo(movie1, movie2 *MovieInfo) int {
+	if movie1.RatingAvg == movie2.RatingAvg {
 		return 0
 	}
-	if data1.RatingAvg > data2.RatingAvg {
+	if movie1.RatingAvg > movie2.RatingAvg {
 		return -1
 	} else {
 		return 1
@@ -91,42 +97,43 @@ func cmpMovieData(data1, data2 MovieData) int {
 }
 
 func (totalizer *RatingTotalizer) updateRatingAverage() {
-	for index := range totalizer.Movies {
-		founded := totalizer.Movies[index]
-		if founded.Count != 0 {
-			totalizer.Movies[index] = *newMovieData(
-				founded.Title,
-				founded.RatingSum,
-				founded.Count,
-				float64(founded.RatingSum)/float64(founded.Count),
-			)
+	for _, movie := range totalizer.Movies {
+		if movie.Count > 0 {
+			movie.RatingAvg = float64(movie.RatingSum) / float64(movie.Count)
 		}
 	}
 }
 
-// After this function is called, the index becomes inconsistent.
-func (totalizer *RatingTotalizer) sort() {
+func (totalizer *RatingTotalizer) getSortedMovies() []*MovieInfo {
 	totalizer.updateRatingAverage()
-	slices.SortFunc(totalizer.Movies, cmpMovieData)
+
+	validMovies := make([]*MovieInfo, 0)
+	for movieID, movie := range totalizer.Movies {
+		_, exists := totalizer.SeenMovies[movieID]
+		if exists && movie.Count > 0 {
+			validMovies = append(validMovies, movie)
+		}
+	}
+
+	slices.SortFunc(validMovies, cmpMovieInfo)
+	return validMovies
 }
 
 func (totalizer *RatingTotalizer) GetTopAndBottom(clientID string, messageId int64, sourceId string) *protopb.TopAndBottomRatingAvg {
-	totalizer.sort()
+	sortedMovies := totalizer.getSortedMovies()
 
-	lastIndex := -1
-	for i := len(totalizer.Movies) - 1; i >= 0; i-- {
-		if totalizer.Movies[i].Count > 0 {
-			lastIndex = i
-			break
-		}
+	if len(sortedMovies) < 2 {
+		return nil
 	}
-	last := totalizer.Movies[lastIndex]
+
+	topMovie := sortedMovies[0]
+	bottomMovie := sortedMovies[len(sortedMovies)-1]
 
 	return &protopb.TopAndBottomRatingAvg{
-		TitleTop:        &totalizer.Movies[0].Title,
-		TitleBottom:     &last.Title,
-		RatingAvgTop:    &totalizer.Movies[0].RatingAvg,
-		RatingAvgBottom: &last.RatingAvg,
+		TitleTop:        proto.String(topMovie.Title),
+		TitleBottom:     proto.String(bottomMovie.Title),
+		RatingAvgTop:    proto.Float64(topMovie.RatingAvg),
+		RatingAvgBottom: proto.Float64(bottomMovie.RatingAvg),
 		ClientId:        proto.String(clientID),
 		MessageId:       proto.Int64(messageId),
 		SourceId:        proto.String(sourceId),
